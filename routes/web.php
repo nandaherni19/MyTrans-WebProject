579be136a3b3c1f5faf9c0da955df77769f080a4
nandaherni19 <nandahernipramudyaningtiyas@gmail.com>
2026-03-26 18:22:14 +0700

    database 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test-db', function () {
+    return DB::table('ms_users')->get();
 });
